cf3ae62ad93426fc2e4dc718178468edc7d057ea
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-09 12:46:02 +0700

    feat: create a function to get authors collection

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,34 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
-import { CreateAuthorDto } from './dto/create-author.dto';
-import { UpdateAuthorDto } from './dto/update-author.dto';
 
 @Controller('authors')
 export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
-  @Post()
-  create(@Body() createAuthorDto: CreateAuthorDto) {
-    return this.authorsService.create(createAuthorDto);
-  }
-
   @Get()
   findAll() {
-    return this.authorsService.findAll();
+    return this.authorsService.getPublishedAuthors();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authorsService.findOne(+id);
+  @Get('/featured')
+  findFeatured() {
+    return this.authorsService.getFeaturedAuthors();
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
-    return this.authorsService.update(+id, updateAuthorDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authorsService.remove(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authorsService.findOne(+id);
+  // }
 }
--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,26 +1,85 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthorDto } from './dto/create-author.dto';
-import { UpdateAuthorDto } from './dto/update-author.dto';
+import axios from 'axios';
+
+import { ItemsService } from 'src/items/items.service';
 
 @Injectable()
 export class AuthorsService {
-  create(createAuthorDto: CreateAuthorDto) {
-    return 'This action adds a new author';
-  }
+  // create(createAuthorDto: CreateAuthorDto) {
+  //   return 'This action adds a new author';
+  // }
 
-  findAll() {
-    return `This action returns all authors`;
-  }
+  // findAll() {
+  //   return `This action returns all authors`;
+  // }
+
+  getPublishedAuthors = async (): Promise<any> => {
+    try {
+      const response = await axios.get(
+        `https://www.chulaseal.com/field/api/collections`,
+      );
+
+      const authors = await response.data;
+
+      const newAuthors = [];
+
+      authors.map((item: any) => {
+        const { id, public: isPublished, element_texts } = item;
+        if (isPublished) {
+          newAuthors.push({
+            id,
+            title: ItemsService.getElementByName('Title', element_texts),
+            description: ItemsService.getElementByName(
+              'Description',
+              element_texts,
+            ),
+          });
+        }
+      });
+
+      return newAuthors;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  getFeaturedAuthors = async (): Promise<any> => {
+    try {
+      const response = await axios.get(
+        `https://www.chulaseal.com/field/api/collections`,
+      );
+
+      const authors = await response.data;
+
+      const newAuthors = [];
+
+      authors.map((item: any) => {
+        const {
+          id,
+          public: isPublished,
+          featured: isFeatured,
+          element_texts,
+        } = item;
+
+        if (isPublished && isFeatured) {
+          newAuthors.push({
+            id,
+            title: ItemsService.getElementByName('Title', element_texts),
+            description: ItemsService.getElementByName(
+              'Description',
+              element_texts,
+            ),
+          });
+        }
+      });
+
+      return newAuthors;
+    } catch (error) {
+      throw error;
+    }
+  };
 
   findOne(id: number) {
     return `This action returns a #${id} author`;
-  }
-
-  update(id: number, updateAuthorDto: UpdateAuthorDto) {
-    return `This action updates a #${id} author`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} author`;
   }
 }
