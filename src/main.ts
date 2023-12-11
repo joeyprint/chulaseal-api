119306c28a5d3e83f3f35755e277b46f54577d6f
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-11 10:02:54 +0700

    feat: fixed enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(3001);
 }
