c7e96219b52c8b32c399378d0ebcf565b7cfaf74
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-14 16:19:04 +0700

    feat: try to enable origin with path

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const webUrl = process.env.WEB_URL;
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', webUrl],
+  });
 
   await app.listen(3001);
 }
