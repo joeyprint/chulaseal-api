ba2d7ee704ffb5c2468f4f91235b1e2448973534
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-14 21:45:52 +0700

    feat: wip enable header

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:3000', 'https://chulaseal.staging.wasin.dev'],
+    allowedHeaders: '*',
+    credentials: true,
   });
 
   await app.listen(3001);
