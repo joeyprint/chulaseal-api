36a818e718f41aaa478b724e5cb3a3c8568e2a13
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-14 16:39:20 +0700

    feat: change origin to star

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const webUrl = process.env.WEB_URL;
 
   app.enableCors({
-    origin: ['http://localhost:3000', webUrl],
+    origin: '*',
   });
 
   await app.listen(3001);
