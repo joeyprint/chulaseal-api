0fc916a533917f797e21352bf25315722d91d98f
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-15 22:36:21 +0700

    feat: update methods

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:3000', 'https://chulaseal.staging.wasin.dev'],
     allowedHeaders: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
 
